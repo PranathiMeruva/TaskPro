9845719bdbc60386b4441d8baa3e00e0dacad4f8
Pranathi Meruva <pranathi.meruva03@gmail.com>
2025-05-29 00:01:47 -0400

    basic setup

---
+++ collab_docs_webapp/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1> My Workspace</h1>
+      <button>click here!</button>
+    </main>
+        
+  );
+}
